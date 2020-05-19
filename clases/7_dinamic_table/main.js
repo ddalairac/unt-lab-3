46640e34dfa7d03678f540a9aa63640b2fb49164
Diego Dalairac <ddalairac@santandertecnologia.com.ar>
2020-05-19 18:34:19 -0300

    dinamic table

---
+++ clases/7_dinamic_table/main.js
@@ -0,0 +1,48 @@
+
+var data = {};
+fetch('./tableData.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .catch(function (error) {
+        console.log('Hubo un problema con la petición Fetch:' + error.message);
+    })
+    .then(function (d) {
+        data = d;
+        console.log('data:', d);
+        let tableElement = Table.render(d);
+        console.log('table:', tableElement);
+        let bodyElement = document.querySelectorAll("body");
+        bodyElement[0].appendChild(tableElement)
+        // renderComponents()
+    });
+
+
+
+export class Table {
+    static render(tableData) {
+        let tableEl = document.createElement('table');
+        tableEl.classList.add("table");
+
+        let theadEl = document.createElement('thead');
+        tableData.head  .forEach(thData => {
+            let thEl = document.createElement('th');
+            thEl.innerHTML = `<p>${thData}</p>`
+            tableEl.appendChild(thEl)
+        });
+        
+        let tbodyEl = document.createElement('tbody');
+        tableData.body.forEach(trData => {
+            let trEl = document.createElement('tr');
+            for (let key in trData) {
+                let tdEl = document.createElement('td');
+                tdEl.innerHTML = `<p>${trData[key]}</p>`
+                trEl.appendChild(tdEl)
+              }
+              tbodyEl.appendChild(trEl)
+        });
+        tableEl.appendChild(tbodyEl)
+
+        return tableEl;
+    }
+}
